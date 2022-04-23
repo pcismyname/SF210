e3b6802d0eca9c3206599b5ae56bcaea345026d0
Your Name <pcismyname007@gmail.com>
2022-04-23 19:52:12 +0700

    cube root

--- cube_root_newton.py
+++ cube_root_newton.py
@@ -1,6 +1,9 @@
+from cmath import sqrt
+
+
 def func(iter,num):
     ans = 0.5
-    for i in range(20):
+    while ans != num**(1/3):
         ans -= ((iter)**3 - num) / (3* ((iter)**(2)))
         iter = ans
     return ans
